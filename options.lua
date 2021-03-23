137ce8119d704797e6b6d384223c51999965cd6d
Rhykes <tyoumans@neo.rr.com>
2021-03-22 18:22:34 -0700

    Battlemaster

--- options.lua
+++ options.lua
@@ -466,7 +466,7 @@ function GSA:OnOptionCreate()
 								inline = true,
 								name = L["General Abilities"],
 								order = 30,					      		
-								args = listOption({195901,214027,34709},"auraApplied"),
+								args = listOption({195901,214027,34709,345231},"auraApplied"),
 							},
 							covenants = { -- AuraApplied
 								type = 'group',
@@ -593,7 +593,7 @@ function GSA:OnOptionCreate()
 								inline = true,
 								name = L["General Abilities"],
 								order = 30,
-								args = listOption({34709},"auraRemoved"),
+								args = listOption({34709,345231},"auraRemoved"),
 							},
 							covenants = { -- AuraRemoved
 								type = 'group',
